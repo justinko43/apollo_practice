653d0214f5e59f97d0dce889dbdad90a1c1d2694
justin.ko43 <justin.ko43@gmail.com>
2019-03-11 11:47:46 -0700

    first commit

---
+++ client/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="main">
+    <div>
+      Initial App Hello
+    </div>
+  </div>
+);
+
+export default App;
---
+++ server/expressApp.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+// Allow cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
+
+// parse request body and cookies
+app.use(bodyParser.json({ limit: '4MB' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('build'));
+
+// our routes
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res) => {
+  console.error(err);
+});
+
+module.exports = app;
